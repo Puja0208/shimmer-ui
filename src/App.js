4bcf037d24094e73dbb930d016d68b525f1ac092
Puja Rani <pujarani@Pujas-MacBook-Air.local>
2025-03-02 15:27:32 +0530

    feat: Create routes and theier compponents

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Body from "./components/Body";
+import About from "./components/About";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Team from "./components/Team";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div>
-      <div className="text-2xl font-bold">Hello world</div>
-      <Body />
+      <div className="text-2xl font-bold py-5 bg-black text-white text-center flex">
+        Hello world
+        <nav className="p-2 m-2 w-96 justify-between text-lg ">
+          <a href="/">Home </a>
+          <a href="/about">About </a>
+          <a href="/team">Team </a>
+          <a href="/login">Login</a>
+        </nav>
+      </div>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1 className="font-bold text-2xl">About</h1>
+      <p>Lorum ipsum</p>
+    </div>
+  );
+};
+
+export default About;
